45f8a7030e9e8b2805f10fc78ceb45c3a0678caa
Yash <163550213+yash-data-tools@users.noreply.github.com>
2026-06-28 10:31:41 +0530

    Update CheckoutPage.jsx

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -35,7 +35,7 @@ export function CheckoutPage({ cart, loadCart }) {
     <>
       <link rel="icon" type="image/svg+xml" href="cart-favicon.png" />
       <title>Checkout</title>
-      <CheckoutHeader />
+      <CheckoutHeader cart={cart} />
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
